8e53b41e3e2d1e96f2bc87213b3f0154cae804d0
Sofie Øyen Haug <sofieoha@stud.ntnu.no>
2023-02-08 17:26:29 +0100

    Fortsatte på oppgave 5
    Co-authored-by: ylvafossan <ylvafossan@users.noreply.github.com>

--- dataStructures/container.py
+++ dataStructures/container.py
@@ -77,7 +77,7 @@ def checkID(id):
 
 
 def randomContainers(containers):
-    for i in range (0,5):
+    for i in range (0,10):
         container = [0,0,0,0,0]
         id = random.randint(0,10000000)
         if checkID(id) == False:
@@ -105,7 +105,7 @@ def fileFormatContainer(container):
 #skrive til fil
 def writeContainersToFile(containers):
     try:
-        with open("dataStructures/load.csv", "w") as fp:
+        with open("load.csv", "w") as fp:
             for container in containers:
                 formatedContainer = fileFormatContainer(container)
                 fp.write("%s\n" % formatedContainer)
@@ -119,7 +119,7 @@ def readContainersFromFile(containers):
     try:
         # fil = open("dataStructures/load.csv", "r") ### NOE SKURRER
         # fil.close()
-        with open("dataStructures/load.csv", "r") as fp:
+        with open("load.csv", "r") as fp:
             container = fp.read()
             list = [container.splitlines()]
             for line in range(0, len(list)-1):
@@ -132,7 +132,7 @@ def readContainersFromFile(containers):
 ### DETT ER SHIPS #####
 
 def NewShip(length, width, height):
-    return [length, width, height, containers]
+    return [length, width, height, []]
 
 #gettere
 def getLengthShip(ship):
@@ -213,7 +213,88 @@ def sortContainersWeight(containers):
 #            
 #             kolonne += 2
 
+def makeDeck(ship):
+    floor = [[[0]*getWidthShip(ship)]*getLengthShip(ship)]
+    for i in range(getHeightShip(ship)-1):
+        deck = []
+        for j in range(getLengthShip(ship)):
+            deck.append([])
+            for k in range(getWidthShip(ship)):
+                deck[j].append(0)
+        floor.append(deck)
+    ship.append(floor)
+    
+def addContainerOnShip(ship,container):
+    place = findPlace(ship,container)
+    if place == False:
+        return False
+    ship[3].append(container)
+    id = getIdContainer(container)
+    deck = place[0][0]
+    row = place[0][1]
+    position1 = place[0][2]
+    if len(place) == 2:
+        position2 = place[1][2]
+        ship[4][deck][row][position1] = id
+        ship[4][deck][row][position2] = id
+    elif len(place) == 1:
+        ship[4][deck][row][position1] = id
+    else: 
+        return False
 
+    return True
+
+def findPlace(ship, container): #rowsene øker ikke. 
+    for i in range(getHeightShip(ship)-1):
+        deck = ship[4][i]
+        if getLengthContainer(container) == 20:
+            for j in range(getWidthShip(ship)):
+                row = deck[j]
+                for k in range(getLengthShip(ship)-1):
+                    position = row[k]
+                    if position == 0 and isContainerBelow(ship, (i, j, k)):
+                       return [[i, j, k]] #doble lister fordi den kjører på to elementer
+        elif getLengthContainer(container) == 40:
+            for j in range(getWidthShip(ship)):
+                row = deck[j]
+                for k in range(getLengthShip(ship)-2):
+                    position1 = row[k]
+                    position2 = row[k+1]
+                    if (position1 == 0 and position2 == 0 and isContainerBelow(ship, (i,j,k)) and isContainerBelow(ship, (i,j,k+1))):
+                        return [[i, j, k],[i, j, k+1]]
+        else:
+            return False  
+
+
+def isContainerBelow(ship, position):
+    deck = position[0]
+    row = position[1]
+    place = position[2]
+    positionBelow = ship[4][deck-1][row][place]
+    if deck == 0:
+        return True
+    elif positionBelow == 0: #there is nothing under here 
+        return False
+    else: 
+        return True
+
+
+randomContainers(containers)
+skip1 = NewShip(6,5,4)
+makeDeck(skip1)
+#print(ship[4][el[0]][el[1]][el[2]])
+sortContainersWeight(containers)
+#containers.reverse()
+for el in containers:
+    print(el)
+    print(addContainerOnShip(skip1,el))
+
+print(skip1[4])
+#print(skip1[4])
+#con = NewContainer(1111,40,3)
+#con1 = NewContainer(1234, 20, 2)
+#print(addContainerOnShip(skip1, con1))
+#print(findPlace(skip1,con))
 
 
 
@@ -255,12 +336,12 @@ def sortContainersWeight(containers):
 #print("------")
 #print(ship)
 
-randomContainers(containers)
-sortContainersWeight(containers)
-for el in containers:
-     print(fileFormatContainer(el))
-writeContainersToFile(containers)
-print("----")
-readContainersFromFile(containers)
-print(containers)
+# randomContainers(containers)
+# sortContainersWeight(containers)
+# for el in containers:
+#      print(fileFormatContainer(el))
+# writeContainersToFile(containers)
+# print("----")
+# readContainersFromFile(containers)
+# print(containers)
 
